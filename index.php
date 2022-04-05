1b56fc11756d5bf098fd1035968c73b5f7aa48cf
rybertallus <101918956+rybertallus@users.noreply.github.com>
2022-04-05 13:53:47 -0400

    Add files via upload

---
+++ index.php
@@ -0,0 +1,59 @@
+<html style="background-image:url('https://www.destinvacationboatrentals.com/wp-content/uploads/2013/04/IMG_3081.jpg');background-size:cover;font-family: geramond;">
+<head>
+<title>Jet Skeets</title>
+</head>
+<body>
+<div>
+<div>
+<div style="display:inline-block;" width="20%">
+<center>
+<h1 style="font-size:40px;color:blue;font-weight:bold;text-align:center;display:inline-block;background-color:#0094FF;transform:rotate(-13deg);">
+Jet Skeets
+</h1>
+<p style="font-size:30px;color:blue;font-style:oblique;text-align:center;background-color:#0094FF;">Jet Skeets provides the cheapest and easiest jet ski rental service on the market. Our innovative technology allows customers
+to not only have more freedom on their jet skis, but allows our prices to be the lowest.</p>
+</center>
+</div>
+<div style="margin-top:-1.5%;">
+<h2 style="text-align:center;color:blue;background-color:#0094FF;">Revolutionary Jet Ski Business</h2>
+</div>
+<div style="text-align:center">
+<div><a href="booktrip.php"><button style="font-size:30px;color:#0094FF;font-weight:bold" class="b1" id="b1">Reserve</button></a></div>
+<div style="margin-top:0.2%"></div>
+<div><a href="app.php"><button style="font-size:30px;color:#0094FF;font-weight:bold" class="b2">App</button></a></div>
+</div>
+</div>
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+<script>
+$(document).ready(function(){
+    $(".b1").hover(function(){
+        $(this).css("font-size", "40px");
+        }, function(){
+        $(this).css("font-size", "30px");
+    });
+	$(".b2").hover(function(){
+        $(this).css("font-size", "40px");
+        }, function(){
+        $(this).css("font-size", "30px");
+    });
+	$(".b3").hover(function(){
+        $(this).css("font-size", "40px");
+        }, function(){
+        $(this).css("font-size", "30px");
+    });
+	$(".b4").hover(function(){
+        $(this).css("font-size", "40px");
+        }, function(){
+        $(this).css("font-size", "30px");
+    });
+});
+</script>
+<div style="margin-top:2%;text-align:center;">
+<div>
+</div>
+
+</div>
+</div>
+</div>
+</body>
+</html>
